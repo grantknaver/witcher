f5603275a6c6bc669d09bfc209e15be4b23dd610
grantknaver <glkfreelance@gmail.com>
2019-06-19 19:06:57 -0700

    sass bootstrap fontAwesome setup finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagesModule, Page1Component } from './pages/pages.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: Page1Component
+  }
+  // {
+  //   path: 'page2',
+  //   component: Page2Component
+  // },
+  // {
+  //   path: 'page3',
+  //   component: Page3Component
+  // },
+  // {
+  //   path: 'page4',
+  //   component: Page4Component
+  // },
+  // {
+  //   path: 'page5',
+  //   component: Page5Component
+  // },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [
+
+  ],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesModule
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+ }
